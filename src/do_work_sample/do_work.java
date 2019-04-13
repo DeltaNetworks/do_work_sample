4fb49ef1211e374916318ad4f905f72b945ec78a
unknown <ronen.yaakobi1@gmail.com>
2019-04-14 00:31:52 +0300

    first files upload

---
+++ src/do_work_sample/do_work.java
@@ -0,0 +1,13 @@
+package do_work_sample;
+
+public class do_work {
+
+	public static void main(String[] args) {
+		
+		print_message prnt = new print_message();
+		prnt.print_loop();
+		prnt.add(5,  3);
+
+	}
+
+}
---
+++ src/do_work_sample/print_message.java
@@ -0,0 +1,20 @@
+package do_work_sample;
+
+public class print_message {
+	
+	public void print_message_1(String msg){
+		System.out.println(msg);
+		
+	}
+	
+	public void print_loop() {
+		for (int i = 0; i < 5; i++) {
+			System.out.println("########   Hello Jenkins   ########");
+		}
+	}
+	
+	public void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+}
